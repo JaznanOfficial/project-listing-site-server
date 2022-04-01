10ceedea88597a022ba72fe2f135b71fd6c36cee
JaznanOfficial <jaznanofficial@gmail.com>
2022-04-01 10:17:49 +0600

    error solved

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -12,7 +13,6 @@ app.use(express.json())
 // pass: XWwn4MzHgqvC19vr
 
 // connecting server------------------------->
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gkhtj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 console.log(uri);
@@ -35,17 +35,18 @@ async function run() {
             const result = await lists.insertOne(list);
             console.log(result);
 
-        // list data get
-            
-            app.get('/lists', async(req, res) => {
-                const cursor = lists.find({});
-                const getList = await cursor.toArray();
-                res.send(getList);
-                console.log(getList);
             })
-    
 
-    })
+        
+        
+            // list data get
+                
+                app.get('/lists', async(req, res) => {
+                    const cursor = lists.find({});
+                    const getList = await cursor.toArray();
+                    res.send(getList);
+                    console.log(getList);
+                })
         
         
         
